b5707a1b88d9f18d6fd79d0d1288030d4cb7f6a9
Expectatives <79179637+Expectatives@users.noreply.github.com>
2022-03-17 14:47:20 -0500

    Update config.js for tickets

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    botClientID: "YOUR_BOT_ID",
+    botPrefix: "!",
+    guildID: "YOUR_GUILD_ID",
+    ownerID: "YOUR_ID",
+    embedColor: "#00dbff",
+    embedfooterText: "Ticket Bot by Expectatives#1157",
+
+    ticketsSupportRoles: [
+        "YOUR_SUPPORT_ROLE_ID_1",
+        "YOUR_SUPPORT_ROLE_ID_2"
+    ],
+    ticketsOpenCategory: "CATEGORY_ID_OPEN",
+    ticketsCloseCategory: "CATEGORY_ID_CLOSE",
+    ticketsTranscripts: "TRANSCRIPT_CHANNEL_ID"
+}
